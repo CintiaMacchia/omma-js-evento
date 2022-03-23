44c9fe6a9ac3ebf8d40f1e26f03cd3cec1130cbc
Cintia Macchia <macchiapaola@hotmail.com>
2022-03-22 21:24:21 -0300

    adicionando js evento com erros

--- index.js
+++ index.js
@@ -2,86 +2,86 @@ const empresa = "Sistema Omma";
 console.log(`Seja bem vindo ao ${empresa}`);
 
 const listaDeReceitas = [{
-  id: 1,
-  titulo: "Cachorro Quente",
-  dificuldade: "simples",
-  ingredientes: ["1 pao de leite", "1 Salsicha", "Maionese"],
-  preparo: "jodbvnujd osbvcujsbn jsbfusb",
-  link: "youtube.com",
-  vegano: false,
+    id: 1,
+    titulo: "Cachorro Quente",
+    dificuldade: "simples",
+    ingredientes: ["1 pao de leite", "1 Salsicha", "Maionese"],
+    preparo: "jodbvnujd osbvcujsbn jsbfusb",
+    link: "youtube.com",
+    vegano: false,
 }, ];
 
 const cadastrarReceita = (
-  id,
-  titulo,
-  dificuldade,
-  ingredientes,
-  preparo,
-  link,
-  vegano
-) => {
-  const novaReceita = {
     id,
     titulo,
     dificuldade,
     ingredientes,
     preparo,
     link,
-    vegano,
-  };
-  listaDeReceitas.push(novaReceita);
+    vegano
+) => {
+    const novaReceita = {
+        id,
+        titulo,
+        dificuldade,
+        ingredientes,
+        preparo,
+        link,
+        vegano,
+    };
+    listaDeReceitas.push(novaReceita);
 
-  console.log(`Cadastro da receita ${titulo} feito com sucesso!`);
+    console.log(`Cadastro da receita ${titulo} feito com sucesso!`);
 };
 
 function exibirReceitas() {
-  for (let index = 0; index < listaDeReceitas.length; index++) {
-    const receita = listaDeReceitas[index];
-    console.log("--------------------------------");
-    console.log("Titulo = " + receita.titulo);
+    for (let index = 0; index < listaDeReceitas.length; index++) {
+        const receita = listaDeReceitas[index];
+        console.log("--------------------------------");
+        console.log("Titulo = " + receita.titulo);
 
-    for (let index = 0; index < receita.ingredientes.length; index++) {
-      const ingrediente = receita.ingredientes[index];
-      console.log("ingredientes = " + ingrediente);
+        for (let index = 0; index < receita.ingredientes.length; index++) {
+            const ingrediente = receita.ingredientes[index];
+            console.log("ingredientes = " + ingrediente);
+        }
+
+        console.log("É vegano? " + receita.vegano);
+        console.log("--------------------------------");
     }
-
-    console.log("É vegano? " + receita.vegano);
-    console.log("--------------------------------");
-  }
 }
 
 // exibirReceitas();
 
 function deletarReceita(id) {
-  let novaListaDeReceitas = [];
+    let novaListaDeReceitas = [];
 
-  for (let index = 0; index < listaDeReceitas.length; index++) {
-    const receita = listaDeReceitas[index];
+    for (let index = 0; index < listaDeReceitas.length; index++) {
+        const receita = listaDeReceitas[index];
 
-    if (receita.id != id) {
-      novaListaDeReceitas.push(receita);
+        if (receita.id != id) {
+            novaListaDeReceitas.push(receita);
+        }
     }
-  }
 
-  if (novaListaDeReceitas.length == listaDeReceitas.length) {
-    return console.log("Não encontrei o id");
-  }
+    if (novaListaDeReceitas.length == listaDeReceitas.length) {
+        return console.log("Não encontrei o id");
+    }
 
-  listaDeReceitas = novaListaDeReceitas;
-  console.log("receita deletada com sucesso!");
+    listaDeReceitas = novaListaDeReceitas;
+    console.log("receita deletada com sucesso!");
 }
 
 function deletarReceita(id) {
-  const novaListaDeReceitas = listaDeReceitas.filter(
-    (receita) => receita.id != id
-  );
+    const novaListaDeReceitas = listaDeReceitas.filter(
+        (receita) => receita.id != id
+    );
 
-  if (novaListaDeReceitas.length == listaDeReceitas.length) {
-    return console.log("Não encontrei o id");
-  }
+    if (novaListaDeReceitas.length == listaDeReceitas.length) {
+        return console.log("Não encontrei o id");
+    }
 
-  listaDeReceitas = novaListaDeReceitas;
-  console.log("receita deletada com sucesso!");
+    listaDeReceitas = novaListaDeReceitas;
+    console.log("receita deletada com sucesso!");
 }
 
 
@@ -89,48 +89,97 @@ const condicaoDeReceita = (receita) => (receita.vegano = true);
 
 
 const atualizaReceita = (id, receitaAtualizada) => {
-  let foiAtualizado = false;
+    let foiAtualizado = false;
 
-  listaDeReceitas.forEach((receita) => {
-    if (receita.id != id) {
-      return;
-    }
+    listaDeReceitas.forEach((receita) => {
+        if (receita.id != id) {
+            return;
+        }
 
-    if (receitaAtualizada.titulo) {
-      receita.titulo = receitaAtualizada.titulo;
-    }
+        if (receitaAtualizada.titulo) {
+            receita.titulo = receitaAtualizada.titulo;
+        }
 
-    if (receitaAtualizada.ingredientes) {
-      receita.ingredientes = receitaAtualizada.ingredientes;
-    }
+        if (receitaAtualizada.ingredientes) {
+            receita.ingredientes = receitaAtualizada.ingredientes;
+        }
 
-    foiAtualizado = true;
-  });
+        foiAtualizado = true;
+    });
 
-  console.log(
-    foiAtualizado ?
-    "Receita atualizada com sucesso!" :
-    "Não foi encontrado o id"
-  );
+    console.log(
+        foiAtualizado ?
+        "Receita atualizada com sucesso!" :
+        "Não foi encontrado o id"
+    );
 };
 
 
 const buscarReceita = (termoBuscado) => {
 
-  const resultados = listaDeReceitas.filter(
-    (receita) =>
-    receita.titulo.toLowerCase().indexOf(termoBuscado.toLowerCase()) != -1
-  );
+    const resultados = listaDeReceitas.filter(
+        (receita) =>
+        receita.titulo.toLowerCase().indexOf(termoBuscado.toLowerCase()) != -1
+    );
 
-  // if (resultados.length) {
-  //   console.log(resultados);
-  // } else {
-  //   console.log("Não foi encontrado receitas");
-  // }
+    // if (resultados.length) {
+    //   console.log(resultados);
+    // } else {
+    //   console.log("Não foi encontrado receitas");
+    // }
 
-  if (!resultados.length) {
-    console.log("Não foi encontrado receitas");
-  }
+    if (!resultados.length) {
+        console.log("Não foi encontrado receitas");
+    }
 
-  return console.log(resultados);
+    return console.log(resultados);
 };
+
+
+//botão enviar
+
+const btnEnviar = document.querySelector('#btnEnviar');
+
+btnEnviar.onclick = (evento) => {
+        //previne evento padrão
+        evento.preventDefault();
+
+        let inputTitulo = document.querySelector('#titulo');
+        let inputDificuldade = document.querySelector('#dificuldade');
+        let inputIngredientes = document.querySelector('#ingredientes');
+        let inputPreparo = document.querySelector('#preparo');
+        let inputLink = document.querySelector('#link');
+        let inputVegano = document.querySelector('input[name="vegano"]:checked');
+
+        let novaReceitaJs = {
+            id: listaDeReceitas.length,
+            titulo: inputTitulo.value,
+            dificuldade: inputDificuldade.value,
+            ingredientes: inputIngredientes.value.split(','),
+            preparo: inputPreparo.value,
+            link: inputLink.value,
+            //vagano: inputVegano.value ? inputVegano: "não"
+        }
+        console.log(novaReceitaJs);
+        listaDeReceitas.push(novaReceitaJs);
+    }
+    //exibir receitas js
+function exibirReceitasJs() {
+    let htmlReceitas = '';
+
+    for (let index = 0; index < listaDeReceitas.length; index++) {
+        htmlReceitas += `<article class="card"> <
+        h2 > $ { listaDeReceitas[index].titulo } </h2> 
+       <small > Dificuldade: $ { listaDeReceitas[index].dificuldade } </small> 
+       < p > $ { listaDeReceitas[index].preparo } </p>  
+        
+            </article>`;
+
+
+
+    }
+    let painelReceitas = document.querySelector('.painel-receitas');
+
+    painelReceitas.innerHTML = htmlReceitas;
+}
+exibirReceitasJs()
